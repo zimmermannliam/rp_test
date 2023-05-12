0d1e1b56064e4ba7ee365492bb66e0466fdf629c
Liam Zimmermann <zimmerml@oregonstate.edu>
2023-05-12 08:12:25 -0700

    refactor tests for submodule/ format

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
-from radiance_pipeline.radiance_data import RadianceData
-from radiance_pipeline.radiance_pipeline import radiance_pipeline
+from submodules.radiance_pipeline.radiance_data import RadianceData
+from submodules.radiance_pipeline.radiance_pipeline import radiance_pipeline
 import sys
 
 def test1():
@@ -12,12 +12,12 @@ def test1():
             target_x_resolution = 1000,
             target_y_resolution = 1000,
             paths_ldr = ["/home/lpz/Downloads/selected_images/Selected_Images/*.JPG"],
-            path_temp = "/home/lpz/school/rp_test/Intermediate/",
-            path_rsp_fn = "/home/lpz/school/rp_test/radiance_pipeline/Inputs/Parameters/Response_function.rsp",
-            path_vignetting = "/home/lpz/school/rp_test/radiance_pipeline/Inputs/Parameters/vignetting_f5d6.cal",
-            path_fisheye = "/home/lpz/school/rp_test/radiance_pipeline/Inputs/Parameters/fisheye_corr.cal",
-            path_ndfilter = "/home/lpz/school/rp_test/radiance_pipeline/Inputs/Parameters/NDfilter_no_transform.cal",
-            path_calfact = "/home/lpz/school/rp_test/radiance_pipeline/Inputs/Parameters/CF_f5d6.cal",
+            path_temp = "/home/lpz/school/rp_test/submodules/Intermediate/",
+            path_rsp_fn = "/home/lpz/school/rp_test/submodules/radiance_pipeline/Inputs/Parameters/Response_function.rsp",
+            path_vignetting = "/home/lpz/school/rp_test/submodules/radiance_pipeline/Inputs/Parameters/vignetting_f5d6.cal",
+            path_fisheye = "/home/lpz/school/rp_test/submodules/radiance_pipeline/Inputs/Parameters/fisheye_corr.cal",
+            path_ndfilter = "/home/lpz/school/rp_test/submodules/radiance_pipeline/Inputs/Parameters/NDfilter_no_transform.cal",
+            path_calfact = "/home/lpz/school/rp_test/submodules/radiance_pipeline/Inputs/Parameters/CF_f5d6.cal",
             path_errors = "",
             path_logs = ""
             )
@@ -34,7 +34,7 @@ def test2():
             target_x_resolution = 1000,
             target_y_resolution = 1000,
             paths_ldr = ["/home/lpz/Downloads/selected_images/Selected_Images/*.JPG"],
-            path_temp = "/home/lpz/school/rp_test/Intermediate/",
+            path_temp = "/home/lpz/school/rp_test/submodules/Intermediate/",
             path_rsp_fn = None,
             path_vignetting = None,
             path_fisheye = None,
